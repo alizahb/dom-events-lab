0464534887a43aa2b14cedb018cbb473a42b21c7
Alizah <alizah.berg77@gmail.com>
2024-05-30 17:19:18 -0400

    calc lab

---
+++ app.js
@@ -0,0 +1,36 @@
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {
+       button.addEventListener('click', () => {
+         //console.log(event.target.innerText);
+         appendNumber(button.innerText);
+         updateDisplay();
+       });
+  
+});
+let numSel = document.querySelectorAll('.button number'.innerText); 
+function appendNumber() {
+
+}
+
+
+
+
+/*
+const calculator = document.querySelector('#calculator');
+calculator.addEventListener('click', (event) => {
+       //console.log(event.target.innerText);
+       const displayEl = document.querySelector('.display'); 
+       if (event.target.innerText = '*') {
+           displayEl.value = '*';
+             
+
+       }
+}); 
+  */
+       
+
+       
+
+     
+      
+     */
